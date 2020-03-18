42dc7bc104896bc814e87a4a465799ff54f28abd
carter3689 <carter3689@gmail.com>
2020-03-18 14:48:22 -0500

    Day 3 Reference code

--- blog2020/__init__.py
+++ blog2020/__init__.py
@@ -2,9 +2,24 @@ from flask import Flask
 
 from config import Config
 
+# Imports for Flask DB and Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Import for Flask Login
+from flask_login import LoginManager
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
 
+# Wrap Our Flask App inside of SQLAlchemy
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
-from blog2020 import routes
+# Login Flow Config
+login = LoginManager(app)
+login.login_view = 'login' # This specifies what page to load for non-authenticated users
+
+
+from blog2020 import routes,models
--- blog2020/routes.py
+++ blog2020/routes.py
@@ -1,8 +1,14 @@
-from blog2020 import app
+from blog2020 import app,db
 from flask import render_template,request,flash,redirect,url_for
 
 # Import for Forms
 from blog2020.forms import SignUpForm,LoginForm,PostForm
+
+# Import for Models
+from blog2020.models import User,check_password_hash
+
+# Import for Flask-Login Modules/Functions
+from flask_login import login_required,login_user, current_user,logout_user
 
 # Home Route
 @app.route("/")
@@ -25,11 +31,16 @@ def home():
 def create_user():
     signupForm = SignUpForm()
     if request.method == 'POST' and signupForm.validate():
+        # Gathering Data From Form
         flash("Thanks for Signing Up!")
         username = signupForm.username.data
         password = signupForm.password.data
         email = signupForm.email.data
         print(username,email,password)
+        # Saving Data To Database
+        user = User(username,email,password)
+        db.session.add(user) # Opening of Database Connection - Staging in memory form data
+        db.session.commit() # Save the data to the database
         return redirect(url_for('login'))
     else:
         flash("The Form has Incorrect Data. Please Try Again")
@@ -44,6 +55,13 @@ def login():
     if request.method == 'POST' and loginForm.validate():
         email = loginForm.email.data
         password = loginForm.password.data
+        logged_user = User.query.filter(User.email == email).first()
+        if logged_user and check_password_hash(logged_user.password, password):
+            login_user(logged_user)
+            print(current_user.username)
+        else:
+            flash('Please Enter Valid Creditials')
+            return redirect(url_for('login'))
         print(email,password)
         return redirect(url_for('home'))
     else:
@@ -53,6 +71,7 @@ def login():
 
 # Post Route for Blog Posts
 @app.route('/post', methods=['GET','POST'])
+@login_required
 def post_blog():
     postForm = PostForm()
     if request.method == 'POST' and postForm.validate():
@@ -64,3 +83,9 @@ def post_blog():
         flash('The information entered is not correct')
         print('Invalid data')
     return render_template('post-form.html', form = postForm)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
